de3896174b46bef7143a3bcb846479fd4ceaf67d
Franko Stipanov <fstipanov@gmail.com>
2021-06-04 10:11:01 +0200

    add ap.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// Selecting elements
+const score1El = document.querySelector('#score--0');
+const score2El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score1El.textContent = 0;
+score2El.textContent = 0;
+diceEl.classList.add('hidden');
